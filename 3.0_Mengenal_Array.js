2c213beab1194a7e3c92c637e0aac787d0843c5a
ahmadngawi <ahmad.ma136new@gmail.com>
2026-09-16 07:55:48 +0700

    first commit

---
+++ 3.0_Mengenal_Array.js
@@ -0,0 +1,16 @@
+// Soal 1
+let bulan = [
+  "Muharram",
+  "Shafar",
+  "Rabi`ul Awwal",
+  "Rabi`ul Akhir",
+  "Jumadil Awwal",
+  "Jumadil Akhir",
+  "Rajab",
+  "Sya`ban",
+  "Ramadhan",
+  "Syawwal",
+  "Dzul Qa`dah",
+  "Dzul Hijjah",
+];
+console.log(bulan[4]);
