6be40062cf524e5a959dfa698947fd0bb793a0e8
ahmadngawi <ahmad.ma136new@gmail.com>
2026-09-16 10:25:11 +0700

    Resolve Soal 9

--- 3.0_Mengenal_Array.js
+++ 3.0_Mengenal_Array.js
@@ -49,3 +49,10 @@ console.log(nilaiSiswaB[0].math); // Menampilkan nilai mate siswa b
 // Soal 8
 angka = [1, 2, 3, 4, 5];
 angka.forEach((a) => console.log(a));
+
+// Soal 9
+let arr1 = ["a", "b", "c"];
+let arr2 = ["d", "e", "f"];
+let arrTergabung = arr1.concat(arr2);
+
+console.log(arrTergabung);
