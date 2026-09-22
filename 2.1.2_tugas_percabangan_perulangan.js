b21e2c93951bfa554d5f4c2ad3caba160dbcc8b3
ahmadngawi <ahmad.ma136new@gmail.com>
2026-09-22 13:51:32 +0700

    end copy code from course (after this, just need for little effort)

--- 2.1.2_tugas_percabangan_perulangan.js
+++ 2.1.2_tugas_percabangan_perulangan.js
@@ -1,5 +1,6 @@
 console.log("Bismillah");
 
+import { log } from "console";
 // Config the interaktif CLI
 import readline from "readline";
 
@@ -13,3 +14,62 @@ function askQuest(query) {
 }
 
 // Login with static password
+
+const password = "123456";
+let orders = [];
+
+async function login() {
+  let userPassword = await askQuest("Enter your password: ");
+
+  if (userPassword === password) {
+    console.log("Login Succesfully!\n");
+    showMenu();
+  } else {
+    console.log("Login Failure...");
+    rl.close();
+  }
+}
+
+// Handle showMenu function
+
+async function showMenu() {
+  console.log("===== MENO UTAMA =====");
+  console.log("1. Buat pesanan baru");
+  console.log("2. Lihat semua pesanan");
+  console.log("3. Perbarui pesanan");
+  console.log("4. Hapus pesanan");
+  console.log("5. Keluar");
+  console.log("======================");
+
+  const choice = await askQuest("Your choice: ");
+  handleMenu(choice);
+}
+
+async function handleMenu(choice) {
+  switch (choice) {
+    // Logic for first choice
+    case "1":
+      break;
+    // Logic for second choice
+    case "1":
+      break;
+    // Logic for third choice
+    case "1":
+      break;
+    // Logic for fourth choice
+    case "1":
+      break;
+    // Logic for fifth choice
+    case "5":
+      "Exit from app...";
+      rl.close();
+      break;
+
+    default:
+      console.log("Command not found...");
+      showMenu();
+      break;
+  }
+}
+
+login();
