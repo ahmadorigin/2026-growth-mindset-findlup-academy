76effec1c996ed9774ad3a0defc239704633cb1c
ahmadngawi <ahmad.ma136new@gmail.com>
2026-09-22 11:26:16 +0700

    buat validasi menjadi ternary

--- 2.1_Percabangan_Login.js
+++ 2.1_Percabangan_Login.js
@@ -1,11 +1,13 @@
 // Latihan 2 -- Validasi Login
 
+const { log } = require("node:console");
+
 // New Command: coba bikin validasi password minimal 6 karakter dengan maksimal 8 karakter...
 // New Command: Buat Function untuk feature login
 
 function validasiLogin(username, password) {
   // Tahap 2: Validasi inputan user
-  if (password.length <= 6 || password.length >= 8) {
+  if (password.length < 6 || password.length > 8) {
     console.log(
       "Password harus lebih dari 5 karakter dan kurang dari 9 karakter",
     );
@@ -13,13 +15,14 @@ function validasiLogin(username, password) {
     return;
   }
 
-  if (username === "admin" && password === "12345678") {
-    // Tahap 3: System check user_password
-    console.log("Login Berhasil!!");
-  } else {
-    console.log("Username atau Password Salahh...");
-  }
+  // Tahap 3: System check user_password
+  const message =
+    username === "admin" && password === "12345678"
+      ? "Login Berhasil!!"
+      : "Username atau Password Salahh...";
+
+  console.log(message);
 }
 
-// Usr input data
-validasiLogin("admin", "1234");
+// User input data
+validasiLogin("admin", "12345678");
