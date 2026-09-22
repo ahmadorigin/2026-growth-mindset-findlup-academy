5a9ec6cff2e5b25d36a2f6813e494d3cb838884f
ahmadngawi <ahmad.ma136new@gmail.com>
2026-09-22 13:37:24 +0700

    Create config for interaktif CLI

--- 2.1.2_tugas_percabangan_perulangan.js
+++ 2.1.2_tugas_percabangan_perulangan.js
@@ -1,1 +1,15 @@
 console.log("Bismillah");
+
+// Config the interaktif CLI
+import readline from "readline";
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+function askQuest(query) {
+  return new Promise((resolve) => rl.question(query, resolve));
+}
+
+// Login with static password
